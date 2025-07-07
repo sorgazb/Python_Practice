cfe76a3599d5b08b37b8278d019cdb5a3cea48cb
sorgazb <sorgazb@gmail.com>
2025-07-07 15:02:36 +0200

    Comienza con la introduccion a Phyton

---
+++ 01-Introduccion/hola_mundo.py
@@ -0,0 +1,3 @@
+# Programa de Hola Mundo con Phyton
+print("Hola Mundo con Python")
+print("Saludos")
---
+++ 01-Introduccion/presentacion.py
@@ -0,0 +1,4 @@
+# Ejercicio de Presentacion con Phyton
+print("Nombre: Sergio Orgaz Bravo")
+print("Edad: 22")
+print("Pais: España")
