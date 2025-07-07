662a497680607cb29bdc4976b03ac039af2b1cca
sorgazb <sorgazb@gmail.com>
2025-07-07 15:57:54 +0200

    Comienza a crear variables en Python

--- 02-Variables/variables.py
+++ 02-Variables/variables.py
@@ -1,4 +1,11 @@
 # Variables en Python
 
 # Declaracion e inicializacion de variables
-edad = 28
+edad = 22
+altura = 1.90
+pais = "España"
+
+# Acceder a las variables
+print("Edad:", edad)
+print("Altura:", altura)
+print("Pais:", pais)
