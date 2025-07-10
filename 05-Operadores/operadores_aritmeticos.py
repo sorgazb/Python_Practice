9c44f58f1c6ab75a04ed5f23ff78f13eeb270571
sorgazb <sorgazb@gmail.com>
2025-07-10 15:41:12 +0200

    Introduccion a los operadores en Python

---
+++ 05-Operadores/operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+# Operadores Aritmeticos
+
+a = 10
+b = 3
+
+# Suma +
+suma = a + b
+print(f"Suma: {suma}")
+
+# Resta -
+resta = a - b
+print(f"Resta: {resta}")
+
+# Multiplicacion *
+multiplicacion = a * b
+print(f"Multiplicacion: {multiplicacion}")
+
+# Division / (devuelve un float)
+division = a / b
+print(f"Division: {division:.2}")
+
+# Division entera //
+division_entera = a // b
+print(f"Division: {division_entera}")
+
+# Modulo %
+modulo = a % b
+print(f"Modulo: {modulo}")
+
+# Potencia **
+potencia = a ** b
+print(f"Potencia: {potencia}")
